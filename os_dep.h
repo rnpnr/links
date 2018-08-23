5ef7f06cecc959968f71be1f603cefed2e492aa6
opask <opask@cock.li>
2018-08-23 06:52:09 -0600

    remove unused #defines in os_dep.h

--- os_dep.h
+++ os_dep.h
@@ -6,17 +6,6 @@
 #ifndef OS_DEP_H
 #define OS_DEP_H
 
-#define SYS_UNIX	1
-#define SYS_OS2		2
-#define SYS_WIN_32	3
-#define SYS_BEOS	4
-#define SYS_RISCOS	5
-#define SYS_ATHEOS	6
-#define SYS_SPAD	7
-#define SYS_INTERIX	8
-#define SYS_OPENVMS	9
-#define SYS_DOS		10
-
 /* hardcoded limit of 10 OSes in default.c */
 
 #ifdef UNIX
@@ -27,7 +16,7 @@
 #if defined(UNIX)
 
 static inline int dir_sep(unsigned char x) { return x == '/'; }
-#define SYSTEM_ID SYS_UNIX
+#define SYSTEM_ID 1
 #define SYSTEM_NAME "Unix"
 #define DEFAULT_SHELL "/bin/sh"
 #define GETSHELL getenv("SHELL")
