437d4efda616a7698d601f5f506b2a1d75e7640d
0x766F6964 <palamar@ualberta.ca>
2020-08-03 16:41:26 -0600

    os_dep.h: small clean up

--- os_dep.h
+++ os_dep.h
@@ -3,25 +3,9 @@
  * This file is a part of the Links program, released under GPL.
  */
 
-#ifndef OS_DEP_H
-#define OS_DEP_H
-
-/* hardcoded limit of 10 OSes in default.c */
-
-#ifdef UNIX
-#undef UNIX
-#endif
-#define UNIX
-
-#if defined(UNIX)
-
 static inline int dir_sep(unsigned char x) { return x == '/'; }
 #define SYSTEM_ID 1
 #define SYSTEM_NAME "Unix"
 #define DEFAULT_SHELL "/bin/sh"
 #define GETSHELL getenv("SHELL")
 #define SHARED_CONFIG_DIR "/etc/"
-
-#endif
-
-#endif /* #ifndef OS_DEP_H */
